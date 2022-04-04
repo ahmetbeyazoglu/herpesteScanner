642fe4bb63c7f01805f3520ede70bdfb1d421102
herpestes <46286059+herpestes0@users.noreply.github.com>
2022-04-04 17:18:24 +0300

    Update herpestes.py

--- herpestes.py
+++ herpestes.py
@@ -3,7 +3,8 @@ import optparse
 import socket
 import dns.resolver, dns.reversename
 from scapy.all import ARP, Ether, srp
-
+import subprocess
+import re
 
 def reverseDns(ip):
     ip2=IP(ip)
@@ -22,6 +23,19 @@ def networkdiscover(net_ip):
     print("IP" + " "*12+"MAC")
     for client in clients:
         print("{:16}    {}".format(client['ip'], client['mac']))
+
+def osdetect(os_ip):
+    p = subprocess.Popen(["ping", "-c", "1",os_ip], stdout=subprocess.PIPE)
+    res=p.communicate()[0]
+    if p.returncode > 0:
+        print('server error')
+    else:
+        pattern = re.compile('ttl=\d*')
+        f = open("ostypes.txt","r")
+        for line in f:
+            a = line.strip().split("-")
+            if a[1] == pattern.search(str(res)).group():
+                print(a[0])
     
 
 
@@ -29,13 +43,15 @@ def main():
     parser = optparse.OptionParser()
     parser.add_option('-r', dest='ip', help='Reversing ip.')
     parser.add_option('-n', dest='net_ip',help='Discover the available divaces same network .')
+    parser.add_option('-o',dest='os_ip',help='Discover target operating system.')
     (options, arguments) = parser.parse_args()
  
     ip = options.ip
     reverseDns(ip)
     net_ip =options.net_ip
     networkdiscover(net_ip)
+    os_ip = options.os_ip
+    osdetect(os_ip)
 
 if __name__ == "__main__":
     main()
- 
